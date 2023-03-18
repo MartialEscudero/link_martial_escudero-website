d45a5d1694534c517d6aa011ca1dbd8a4080b9ca
Martial Escudero <martial.escudero@gmail.com>
2023-03-18 15:26:32 +0100

    remove strapi api

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,72 @@
-<script>
-export default {
-    data() {
-        return {
-            socialsTab: [],
-        }
-    },
-
-    methods: {
-        async socialCreated() {
-            const response = await fetch("https://strapi.martialescudero.com/api/links?filters[ShowIt][$eq]=true&sort=id");
-            const dataSocial = await response.json();
-
-            dataSocial.data.forEach((element) => {
-                this.socialsTab.push({
-                    title: element.Title,
-                    description: element.attributes.Description,
-                    url: element.attributes.Url,
-                    borderColor:
-                        "border: 3px solid " +
-                        element.attributes.Color,
-                    styleColor:
-                        "color :" + element.attributes.Color,
-                    icon: element.attributes.Icon,
-                });
-            });
-        },
-    },
-
-    mounted() {
-        this.socialCreated();
-    }
-}
+<script setup>
+const socialMedia = [{
+    "icon": "link-outline",
+    "title": "Site Web",
+    "description": "Venez découvrir mon portfolio personnel.",
+    "color": "#93c5fd",
+    "url": "https://www.martialescudero.com",
+}, {
+    "icon": "document-outline",
+    "title": "Mon CV",
+    "description": "Retrouvez mon CV en PDF.",
+    "color": "#FE7A56",
+    "url": import.meta.env.VITE_CV_URL,
+}, {
+    "icon": "logo-github",
+    "title": "GitHub",
+    "description": "Venez découvrir mes projets.",
+    "color": "#FFFFFF",
+    "url": "https://github.com/MartialEscudero",
+}, {
+    "icon": "logo-linkedin",
+    "title": "LinkedIn",
+    "description": "Retrouvez mon profil LinkedIn.",
+    "color": "#5476F1",
+    "url": "https://www.linkedin.com/in/martial-escudero",
+}, {
+    "icon": "logo-behance",
+    "title": "Behance",
+    "description": "Retrouvez mes anciens projets audiovisuels.",
+    "color": "#1D4CF9",
+    "url": "https://www.behance.net/pureproduction",
+}]
 </script>
 
 <template>
     <div class="flex flex-col items-center">
-        <div>
-            <a href="https://www.martialescudero.com" target="_blank">
-                <img data-aos="fade" class="mx-auto w-28 md:w-40 lg:w-52 xl:my-28 mt-10 mb-10 md:mb-16" src="./assets/img/icon.png" />
-            </a>
-            <div data-aos="fade" class="mx-auto grid gap-10 md:gap-16 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-6 mb-20">
-                <div v-for="social in socialsTab">
-                    <a :href="social.url" target="_blank">
-                        <div class="card flex items-center text-white transform transition duration-300 hover:scale-90 max-w-xs h-48" :style="social.borderColor">
-                            <div>
-                                <ion-icon class="mb-2 md:mb-4" :name="social.icon"/>
-                                <h1 class="text-2xl">{{ social.title }}</h1>
-                                <p class="text-base">
-                                    {{ social.description }}
-                                </p>
-                            </div>
-                        </div>
-                    </a>
-                </div>
+        <a href="https://www.martialescudero.com" target="_blank">
+            <img data-aos="fade" class="mx-auto w-28 md:w-40 lg:w-52 xl:my-28 mt-10 mb-10 md:mb-16" src="./assets/img/icon.png" />
+        </a>
+        <div data-aos="fade" class="mx-auto grid gap-10 md:gap-16 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 px-6 mb-20">
+            <div v-for="social in socialMedia">
+                <a :href="social.url" target="_blank">
+                    <div class="card flex flex-col items-start text-white transform transition duration-300 hover:scale-105 max-w-xs h-48" :style="`border: 3px solid ${social.color}`">
+                        <ion-icon class="mb-2 md:mb-4" :name="social.icon"/>
+                        <h1 class="text-2xl">{{ social.title }}</h1>
+                        <p class="text-base">{{ social.description }}</p>
+                    </div>
+                </a>
             </div>
         </div>
     </div>
 </template>
+
+<style>
+ion-icon {
+    transition: 0.3s;
+    font-size: 50px;
+}
+
+body {
+    background-color: #0e0e0e;
+    font-family: "Poppins", sans-serif;
+}
+
+.card {
+    background-color: #202020;
+    border: 3px solid white;
+    border-radius: 15px;
+    padding: 20px;
+    cursor: pointer;
+}
+</style>
