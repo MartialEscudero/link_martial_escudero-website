eec17812a2d9629c5ddd581318e5f7e7aaaa7614
Martial Escudero <martial.escudero@gmail.com>
2023-08-28 15:33:03 +0200

    refactor code

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue"
+import App from "./App.vue"
 
-import './assets/css/mains.css'
+import "./assets/css/mains.css"
 
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 AOS.init();
 
-createApp(App).mount('#app')
+createApp(App).mount("#app")
